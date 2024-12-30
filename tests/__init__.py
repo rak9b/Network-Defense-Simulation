ec1455a1f21d42daf97d9ec08fc5b66d5662ad15
Rakib 007 <152210130+rak9b@users.noreply.github.com>
2024-12-30 20:44:28 +0600

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+"""
+Tests Module
+
+This module contains unit tests for various components of the Network Defense Simulation project.
+
+Structure:
+    - test_firewall: Tests for the FirewallManager class.
+    - test_ids: Tests for the IDSMonitor class.
+
+Author:
+    Your Name
+
+Version:
+    1.0.0
+"""
+
+# Define the package version
+__version__ = "1.0.0"
